50d0bfa86c6aad7a01cb09f4035711f8c337eef1
Giovanni Chierico <giovanni.chierico@gmail.com>
2021-12-16 06:54:18 +0100

    fix textarea editing

--- TODO.md
+++ TODO.md
@@ -1,4 +1,5 @@
 TODO backlog
+* refresh NoteSetsQueue to make it coherent with current note (not with next)
 * add parsing notes with octave
 * implement speed-up
 * implement ability to use different patterns (not just one up and one down)
@@ -8,12 +9,9 @@ TODO backlog
 * make UI pretty
 * display notes in proper notation
 * fix test config so it can run the `npm test` and `npm start` from the same configuration
-* where the core html page that loads the scripts and has the root item?
+* where is the core html page that loads the scripts and has the root item?
 * fix css for buttons when clicked
 * possibility to have scales without the root note
-* link chord names to scale modes
-* loop over chords instead of note sets
 * transform empty components into custom hooks?
 * add possibility to define custom scale (using steps? note?)
-* fix textarea for mapping, cursor should remain where it is when refreshed
 * use Tone.Loop instead of Clock
--- src/UI/ChordMappingUI.tsx
+++ src/UI/ChordMappingUI.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { ChordMapping, ChordMappingGlobal } from "../musicEngine/ChordMappingParser";
 
 const DEFAULT_MAPPING_TEXT = `7+: mode 1 of Major
@@ -19,28 +19,44 @@ export function ChordMappingGlobalUI({ setChordMappingGlobal: setParentChordMapp
     const [chordMappingGlobal, setChordMappingGlobal] =
         useState<ChordMappingGlobal>(ChordMappingGlobal.parse(DEFAULT_MAPPING_TEXT));
 
+    const refTextarea = useRef();
+
+    // set up values the first time only
     useEffect(() => {
         setText(chordMappingGlobal.toString());
         setError('');
         setParentChordMappingGlobal(chordMappingGlobal);
-    }, [chordMappingGlobal, setText, setError]);
+    }, []);
+
+    useEffect(() => {
+        // setText(chordMappingGlobal.toString());
+        setError('');
+        setParentChordMappingGlobal(chordMappingGlobal);
+    }, [chordMappingGlobal, setError]);
 
     const onChange = useCallback((ev: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const textValue = ev.target.value;
         try {
-            const parsed = ChordMappingGlobal.parse(ev.target.value);
+            const parsed = ChordMappingGlobal.parse(textValue);
             setChordMappingGlobal(parsed);
-            return true;
         } catch (e) {
-            setText(ev.target.value);
             if (e instanceof Error) {
                 setError(e.message);
             }
         }
+        // keep the test the same in case if comes from user input
+        setText(textValue);
     }, [setError, setChordMappingGlobal]);
 
     return (<div className="row">
         <div className="col">
-            <textarea className="form-control" onChange={onChange} value={text} rows={chordMappingGlobal.mappings.length + 1} />
+            <textarea
+                ref={refTextarea.current}
+                className="form-control"
+                onChange={onChange}
+                value={text}
+                rows={chordMappingGlobal.mappings.length + 1}
+            />
             <div className="col">{error}</div>
         </div>
         <div className="col">{chordMappingGlobal.mappings.map((chordMapping) => {
